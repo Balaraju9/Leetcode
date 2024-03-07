86a87a39ec28e6c2f98d74987a887c998f906903
balaraju <119840117+Balaraju9@users.noreply.github.com>
2024-03-07 21:49:26 +0530

    Time: 7 ms (40.67%) | Memory: 8.2 MB (84.70%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+
+    string reverseWords(string s) {
+        int i=0,j=0,c=0,c1=0,c2=0;
+        while(s[i]==' '){
+            i++;
+        }
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=' '){
+                c1++;
+            }
+        }
+        while(i<s.size()){
+            if(s[i]!=' '){
+                if(c2==c1){
+                    break;
+                }
+                if(c==1){
+                    s[j]=' ';
+                    j++;
+                    c=0;
+
+                }
+                else{
+                s[j]=s[i];
+                i++;
+                j++;
+                c2++;
+                }
+            }
+            else{
+            i++;
+            c=1;
+            
+            }
+        }
+        int pu=0,pu1=0;
+        for(int i=0;i<s.size();i++){
+            if(pu==c1){
+                break;
+            }
+            else if(s[i]!=' '){
+                pu++;
+
+            }
+            else{
+                pu1++;
+            }
+
+        }
+        s.resize(pu+pu1);
+        i=0;
+        while(i<s.size()){
+            if(s[i]!=' ' ){
+
+                j=i;
+                
+                while(s[i]!=' '&& i<s.size()){
+                    i++;
+                }
+                int t1=j,t2=i-1;
+                while(t1<=t2){
+                   swap(s[t1],s[t2]);
+                    t1++;
+                    t2--;
+                }
+            }
+            else{
+                i++;
+            }
+            
+        }
+        reverse(s.begin(),s.end());
+        
+        return s;
+        
+    }
+};
