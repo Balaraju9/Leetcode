0002ada3098919a9c4e3710d4f491166c9a8d401
balaraju <119840117+Balaraju9@users.noreply.github.com>
2024-03-11 11:42:19 +0530

    Time: 4 ms (26.39%) | Memory: 8.6 MB (20.54%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string customSortString(string o, string s) {
+        map<int,char> mp;
+        map<char,int> mp1;
+        string s2="";
+        for(int i=0;i<o.size();i++){
+            mp[i]=o[i];
+        }
+        for(int i=0;i<s.size();i++){
+            if(o.find(s[i])==string::npos){
+                s2+=s[i];
+            }
+        }
+        cout<<s2;
+        for(int i=0;i<s.size();i++){
+           
+                mp1[s[i]]++;
+            
+
+        }
+         
+        vector<pair<char,int>> v1;
+        for(auto &i:mp){
+            for(auto &j:mp1){
+                if(i.second==j.first){
+                    v1.push_back(pair<char,int>{j.first,j.second});
+                }
+
+            }
+        }
+        string s1="";
+       for(auto &i:v1){
+           while(i.second!=0){
+               s1+=i.first;
+               i.second--;
+           }
+       }
+       s1+=s2;
+        return s1;
+     
+
+       
+       
+
+        
+    }
+};
