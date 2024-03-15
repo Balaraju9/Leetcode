a2aac4098ae2f1ae301d0be5536c4ff3d62a3a20
balaraju <119840117+Balaraju9@users.noreply.github.com>
2024-03-15 20:36:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (23.48%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -4,20 +4,6 @@ public:
           ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-       vector<long long> v1;
-       v1.push_back(1);
-       
-       int i=1;
-
-       while(n--){
-        v1.push_back(4*i);
-        i++;
-
-       
-       }
-       for(int i=1;i<v1.size();i++){
-        v1[i]=v1[i-1]+v1[i];
-       }
-       return v1[v1.size()-2];
+       return pow(n,2)+pow(n-1,2);
     }
 };
