4aa7d1931f9fdec4f6e7f101b496a9f3665cbf25
balaraju <119840117+Balaraju9@users.noreply.github.com>
2024-03-16 20:08:35 +0530

    Time: 119 ms (13.78%) | Memory: 86.8 MB (91.02%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& n) {
+        for(int i=0;i<n.size();i++){
+            if(n[i]==0){
+                n[i]=-1;
+            }
+        }
+        int max1=0,s=0;
+        map<int,int> mp;
+        for(int i=0;i<n.size();i++){
+            s=s+n[i];
+            if(s==0){
+                max1=max(max1,i+1);
+            }
+            else if(mp.find(s)!=mp.end()){
+                int k=i-mp[s];
+                max1=max(max1,k);
+            }
+            else{
+                mp[s]=i;
+            }
+            
+        }
+        return max1;
+        
+    }
+};
