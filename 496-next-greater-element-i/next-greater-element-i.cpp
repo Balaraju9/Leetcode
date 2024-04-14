a6c1c39d5317f00972f9f110ff769b62b62b49f8
balaraju <119840117+Balaraju9@users.noreply.github.com>
2024-04-14 09:57:29 +0530

    Time: 3 ms (84.40%) | Memory: 12.1 MB (27.10%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -2,41 +2,26 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& n1, vector<int>& n2) {
         stack<int> st;
-        vector<int> v1;
-        for(int i=n2.size()-1;i>=0;i--){
-              while(!st.empty()){
-                    if(st.top()>n2[i]){
-                        break;
-                    }
-                    st.pop();
-                }
-            if(st.empty()){
-                v1.push_back(-1);
-                st.push(n2[i]);
-            }
-            else if(st.top()>n2[i]){
-                v1.push_back(st.top());
-                st.push(n2[i]);
-
-            }
-           
-              
-             
-               
-            
-            
-        }
-        
-        reverse(v1.begin(),v1.end());
         unordered_map<int,int> mp;
-       for(int i=0;i<v1.size();i++){
-           mp[n2[i]]=v1[i];
+        for(int i=n2.size()-1;i>=0;i--){
+           while(!st.empty() && n2[i]>st.top()){
+            st.pop();
 
-       }
-       for(int i=0;i<n1.size();i++){
-           n1[i]=mp[n1[i]];
-       }
-        return n1;
+           }
+           if(st.empty()){
+            mp[n2[i]]=-1;
+            st.push(n2[i]);
+           }
+           else{
+            mp[n2[i]]=st.top();
+           
+            st.push(n2[i]);
+           }
+        }
+    for(int i=0;i<n1.size();i++){
+        n1[i]=mp[n1[i]];
+    }
+    return n1;
         
     }
 };
