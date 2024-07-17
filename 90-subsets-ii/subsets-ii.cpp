f5ae8c9eb8de307b285a7d96b74ff90da36a6caa
balaraju <119840117+Balaraju9@users.noreply.github.com>
2024-07-17 16:20:11 +0530

    Time: 13 ms (6.06%) | Memory: 16.8 MB (5.10%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
+    void fun( vector<int>& n, vector<vector<int>> &v1,vector<int> v2, int i){
+        if(i==n.size()){
+            v1.push_back(v2);
+            return;
+        }
+        v2.push_back(n[i]);
+        fun(n,v1,v2,i+1);
+        v2.pop_back();
+        fun(n,v1,v2,i+1);
+    }
     vector<vector<int>> subsetsWithDup(vector<int>& n) {
         map<vector<int>,int> mp;
-        vector<vector<int>> v2;
-        vector<int> v1;
-        for(int i=0;i<pow(2,n.size());i++){
-            for(int j=0;j<n.size();j++){
-                if(i&(1<<j)){
-                    v1.push_back(n[j]);
-                }
+        vector<vector<int>> v1;
+        vector<int> v2;
+        int i=0;
+        fun(n,v1,v2,i);
+        vector<vector<int>> v4;
+        for(auto &i:v1){
+            sort(i.begin(),i.end());
+            if(mp.find(i)==mp.end()){
+                v4.push_back(i);
+                mp[i]++;
             }
-            sort(v1.begin(),v1.end());
-            mp[v1]++;
-            v1.clear();
+            
         }
-        for(auto &i:mp){
-           v2.push_back(i.first);
-        }
-        return v2;
+        return v4;
         
     }
 };
