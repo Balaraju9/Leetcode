c2f74eb4c8f76a91f605d16026cf1aea12a277b8
balaraju <119840117+Balaraju9@users.noreply.github.com>
2024-10-04 11:31:36 +0530

    Time: 71 ms (23.00%) | Memory: 56.3 MB (84.20%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -3,20 +3,18 @@ public:
     long long dividePlayers(vector<int>& s) {
         sort(s.begin(),s.end());
         long long s1=0;
-        vector<long long> v1;
+       unordered_map<int,int> mp;
         for(long long i=0;i<s.size()/2;i++){
             s1+=s[i]*s[s.size()-i-1];
-            v1.push_back(s[i]+s[s.size()-i-1]);
+           mp[(s[i]+s[s.size()-i-1])]++;
+           if(mp.size()>1){
+            return -1;
+           }
            
 
 
         }
-        int fi=v1[0];
-        for(int i=0;i<v1.size();i++){
-            if(v1[i]!=fi){
-                return -1;
-            }
-        }
+        
         return s1;
         
     }
