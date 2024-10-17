c04285e7c4271555a251424a981cccf6c32def8f
balaraju <119840117+Balaraju9@users.noreply.github.com>
2024-10-17 20:04:08 +0530

    Time: 4 ms (99.99%) | Memory: 22.9 MB (88.36%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& arr) {
-        if(arr.size()==1){
-            return arr;
-        }
-        sort(arr.begin(),arr.end());
-        for(int i=1;i<arr.size();i++){
-            if(arr[i-1][1]>=arr[i][0] && arr[i-1][1]<=arr[i][1]){
-                arr[i][0]=arr[i-1][0];
-                arr[i-1]={};
-            }
-            else if(arr[i-1][1]>arr[i][0] && arr[i-1][1]>arr[i][1]){
-                arr[i]=arr[i-1];
-                arr[i-1]={};
-            }
-        }
+    vector<vector<int>> merge(vector<vector<int>>& v1) {
+        sort(v1.begin(),v1.end());
         vector<vector<int>> v2;
-        for(auto &i:arr){
-            if(i.size()>0){
-                v2.push_back(i);
+        for(int i=1;i<v1.size();i++){
+            int cur1=v1[i][0];
+            int cur2=v1[i][1];
+            int pr1=v1[i-1][0];
+            int pr2=v1[i-1][1];
+            if(cur1>=pr1 && cur1<=pr2){
+                v1[i][0]=min(cur1,pr1);
+                v1[i][1]=max(cur2,pr2);
+                v1[i-1][0]=-919;
+                v1[i-1][1]=-919;
+            }
+        }
+        for(int i=0;i<v1.size();i++){
+            if(v1[i][0]==-919){
+
+            }
+            else{
+                v2.push_back({v1[i][0],v1[i][1]});
             }
         }
         return v2;
-
         
     }
 };
