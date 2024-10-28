4b708ba4a3f331144c20a6cb6a2618e872924255
balaraju <119840117+Balaraju9@users.noreply.github.com>
2024-10-28 07:06:44 +0530

    Time: 83 ms (68.89%) | Memory: 130.5 MB (13.83%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
     int longestSquareStreak(vector<int>& n) {
-        long long int max1 = 0;
-        for(int i = 0; i < n.size(); i++) {
-           if(max1 < n[i]) {
-               max1 = n[i];
+        vector<long long int> v1;
+        for(auto &i:n){
+            v1.push_back(i);
+        }
+        long long int max1=0;
+        for(int i=0;i<v1.size();i++){
+           if(max1<v1[i]){
+            max1=v1[i];
            }
         }
-
-        unordered_map<long long int, long long int> mp;
-        for(int i = 0; i < n.size(); i++) {
+        
+        unordered_map<long long int,long long int> mp;
+        for(int i=0;i<n.size();i++){
             mp[n[i]]++;
         }
-
-        int ans = -1;
-        for(int i = 0; i < n.size(); i++) {
-            if(max1 >= static_cast<long long>(n[i]) *static_cast<long long>(n[i]) && mp.find(static_cast<long long>(n[i]) * static_cast<long long>(n[i])) != mp.end()) {
-                int c = 0;
-                long long k = static_cast<long long>(n[i]) * static_cast<long long>(n[i]);
-                while(mp.find(k) != mp.end()) {
+        int ans=-1;
+        for(int i=0;i<n.size();i++){
+            if(max1>=(v1[i]*v1[i]) && mp.find(v1[i]*v1[i])!=mp.end()){
+                int c=0;
+                long long  k=v1[i]*v1[i];
+                while(mp.find(k)!=mp.end()){
                     c++;
-                    k = k * k;
+                    k=k*k;
+
                 }
-                ans = max(ans, c + 1);
+                ans=max(ans,c+1);
+
             }
+
+
         }
         return ans;
     }
