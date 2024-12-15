3f28dccec51aff9730ea1d2a992fc02c3872a6db
balaraju <119840117+Balaraju9@users.noreply.github.com>
2024-12-15 12:16:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (7.67%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    int minBitFlips(int s, int g) {
-        string k=bitset<128>(s).to_string();
-        string k1=bitset<128>(g).to_string();
-        int i=k.size()-1,j=k1.size()-1;
-        int ans=0;
-       while(i>=0 && j>=0){
-        ans+=(abs((k[i]-'0')-(k1[i]-'0')));
-        i--;
-        j--;
-
-       }
-       return ans;
+    int minBitFlips(int st, int g) {
+        string s1=bitset<128>(st).to_string();
+        string s2=bitset<128>(g).to_string();
+        int c=0;
+        for(int i=0;i<s1.size();i++){
+            if(s1[i]!=s2[i]){
+                c++;
+            }
+        }
+        return c;
         
     }
 };
