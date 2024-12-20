e440af607c8571ce7e196c90c8bdb7aea3d79a82
balaraju <119840117+Balaraju9@users.noreply.github.com>
2024-12-20 11:41:11 +0530

    Time: 0 ms (100.00%) | Memory: 77.1 MB (70.74%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
@@ -6,53 +6,26 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
- void fun1(TreeNode* r, int le, map<int, vector<int>>& mp) {
-        if (r == NULL) {
+    void fun(TreeNode* r,TreeNode* r1,int l1,int l2){
+        if(r==NULL){
             return;
         }
-        if (le % 2 != 0) {
-            vector<int> v1=mp[le];
-          
-            r->val=v1[v1.size()-1];
-           
-            mp[le].pop_back();
+        if(l1%2==0){
+           swap(r1->val,r->val);
             
         }
-        fun1(r->right, le + 1, mp);
-        fun1(r->left, le + 1, mp);
-    }
-    void fun(TreeNode* r, int le, map<int, vector<int>>& mp) {
-        if (r == NULL) {
-            return;
-        }
-        if (le % 2 != 0) {
-            mp[le].push_back(r->val);
-        }
-        fun(r->left, le + 1, mp);
-        fun(r->right, le + 1, mp);
+        fun(r->left,r1->right,l1+1,l2+1);
+        fun(r->right,r1->left,l1+1,l2+1);
     }
     TreeNode* reverseOddLevels(TreeNode* r) {
-        map<int, vector<int>> mp;
-
-        fun(r, 0, mp);
-       
-
-        for (auto& i : mp) {
-           
-            vector<int> v2 = i.second;
-            reverse(v2.begin(),v2.end());
-            i.second=v2;
-
-           
-        }
-       
-         fun1(r,0,mp);
+        TreeNode* r1=r;
+        fun(r->right,r->left,0,0);
         return r;
+        
     }
 };
