f5c3fc608035ef642425840a4f959b394c73b722
balaraju <119840117+Balaraju9@users.noreply.github.com>
2025-01-12 21:18:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (74.92%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
-    int minBitFlips(int st, int g) {
-        string s1=bitset<128>(st).to_string();
-        string s2=bitset<128>(g).to_string();
-        int c=0;
-        for(int i=0;i<s1.size();i++){
-            if(s1[i]!=s2[i]){
-                c++;
-            }
-        }
-        return c;
+    int minBitFlips(int s, int g) {
+        int pu=s^g;
+        return __builtin_popcount(pu);
+        
         
     }
 };
