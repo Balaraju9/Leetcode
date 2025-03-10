35023ccb6166bbbce841bec0bf959ead88ad1c0e
balaraju <119840117+Balaraju9@users.noreply.github.com>
2025-03-10 14:12:43 +0530

    Time: 0 ms (100.00%) | Memory: 53.2 MB (13.46%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     bool canJump(vector<int>& n) {
-    vector<bool> v1(n.size());
-    v1[n.size()-1]=true;
-    for(int i=n.size()-2;i>=0;i--){
-        if(n[i]==0){
-            v1[i]=false;
+        if(n.size()<=1){
+            return true;
         }
-        else{
-      for(int j=i;j<=i+n[i];j++){
-        if(v1[j]==1){
-            v1[i]=1;
-            break;
-        }
-      }
-        }
-    
+        vector<bool> v1(n.size(),false);
+        int reach=n.size()-1;
+        for(int i=n.size()-1;i>=0;i--){
+            if(i+n[i]>=reach){
+                v1[i]=true;
+                reach=i;
 
 
-    }
-    return v1[0];
-
+            }
+        }
+        return v1[0];
         
     }
 };
