474bde2f66e80c84f02ccc05d353d1485fe922ca
balaraju <119840117+Balaraju9@users.noreply.github.com>
2025-03-14 10:24:56 +0530

    Time: 81 ms (5.02%) | Memory: 149.7 MB (5.02%) - LeetSync

---
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool fun(vector<int> cp,long long k,int m){
+        for(int i=cp.size()-1;i>=0;i--){
+            if(cp[i]>=m){
+                k=k-(cp[i]/m);
+            }
+            if(k<=0) return true;
+        }
+        return false;
+
+    }
+    int maximumCandies(vector<int>& c, long long k) {
+        sort(c.begin(),c.end());
+        int l=1;
+        int h=c[c.size()-1];
+        vector<int> c1=c;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(fun(c1,k,m)==true){
+                l=m+1;
+            }
+            else{
+                h=m-1;
+            }
+        }
+        return h;
+        
+        
+    }
+};
