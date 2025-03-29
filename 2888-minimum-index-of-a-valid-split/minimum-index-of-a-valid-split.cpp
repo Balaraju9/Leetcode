8a5e9e9bd8d28df32293af4a5a4c920ce386e79b
balaraju <119840117+Balaraju9@users.noreply.github.com>
2025-03-29 16:19:37 +0530

    Time: 2339 ms (5.04%) | Memory: 110.7 MB (5.02%) - LeetSync

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& n) {
+        unordered_map<int,int> mp;
+        int pre=0;
+        int cur=0,cnt=0;
+        vector<int> v1(n.size());
+        vector<int> v2(n.size());
+        for(int i=0;i<n.size();i++){
+            mp[n[i]]++;
+            if(cnt==0){
+                pre=n[i];
+
+            }
+            else if(n[i]==pre){
+                cnt++;
+
+            }
+            else{
+                cnt--;
+            }
+            if(mp[pre]>(i+1)/2 ){
+                v1[i]=pre;
+
+            }
+
+        }
+        mp.clear();
+        cnt=0,pre=0;
+             for(int i=n.size()-1;i>=0;i--){
+            mp[n[i]]++;
+            if(cnt==0){
+                pre=n[i];
+                cnt=1;
+
+            }
+            else if(n[i]==pre){
+                cnt++;
+
+            }
+            else{
+                cnt--;
+            }
+            if(mp[pre]>(n.size()-i)/2 ){
+                v2[i]=pre;
+
+            }
+
+        }
+        for(auto &i:v1){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        for(auto &i:v2){
+            cout<<i<<" ";
+        }
+        for(int i=0;i<v1.size()-1;i++){
+            if(v1[i]==v2[i+1] && v1[i]!=0){
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+};
