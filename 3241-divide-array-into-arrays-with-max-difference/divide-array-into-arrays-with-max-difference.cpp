8776206306b8499c6ef7567053bb6980a7900518
balaraju <119840117+Balaraju9@users.noreply.github.com>
2025-06-18 19:57:13 +0530

    Time: 50 ms (53.59%) | Memory: 120.3 MB (28.47%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -2,32 +2,26 @@ class Solution {
 public:
     vector<vector<int>> divideArray(vector<int>& n, int k) {
         sort(n.begin(),n.end());
-        vector<vector<int>> v1;
-        vector<int> v2;
+        vector<vector<int>> v2;
+        vector<int> v1;
         int c=0;
-        for(int i=0;i<n.size();i++){
-            if(c==3){
-                v1.push_back(v2);
-                c=0;
-                v2.clear();
-            }
-            v2.push_back(n[i]);
-            c++;
 
-        }
-        v1.push_back(v2);
-       
-        
-        for(int i=0;i<v1.size();i++){
-            if((v1[i][1]-v1[i][0])<=k && (v1[i][2]-v1[i][1])<=k && (v1[i][2]-v1[i][0])<=k){
-                continue;
+        for(auto &i:n){
+            c++;
+            v1.push_back(i);
+            if(c==3){
+                c=0;
+                v2.push_back(v1);
+                v1.clear();
             }
-            else{
+        }
+        for(int i=0;i<v2.size();i++){
+            if(v2[i][2]-v2[i][0]>k){
                 return {};
+
             }
         }
-        return v1;;
-        
+        return v2;
         
     }
 };
