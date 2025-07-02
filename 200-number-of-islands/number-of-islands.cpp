87257624aab830c61a3583f5016851b0e43bfc18
balaraju <119840117+Balaraju9@users.noreply.github.com>
2025-07-02 10:12:58 +0530

    Time: 28 ms (52.32%) | Memory: 24.2 MB (23.57%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,43 +1,45 @@
 class Solution {
 public:
-    void fun(vector<vector<char>> g,vector<vector<char>> &vis,int i,int j){
-        vis[i][j]='1';
+   void fun(vector<vector<char>> &g,vector<vector<int>> &vis,int i,int j){
+
         queue<pair<int,int>> q;
         q.push({i,j});
+      vis[i][j]=1;
         while(!q.empty()){
-            auto front=q.front();
+            auto [x,y]=q.front();
+            
             q.pop();
-            int i1=front.first;
-            int j1=front.second;
-            if(i1+1<g.size() && vis[i1+1][j1]!='1' && g[i1+1][j1]=='1'){
-                q.push({i1+1,j1});
-                vis[i1+1][j1]='1';
+            if(x-1>=0 && g[x-1][y]=='1' && vis[x-1][y]!=1){
+                q.push({x-1,y});
+                vis[x-1][y]=1;
+
             }
-             if(i1-1>=0 && vis[i1-1][j1]!='1' && g[i1-1][j1]=='1'){
-                q.push({i1-1,j1});
-                 vis[i1-1][j1]='1';
+            if(y-1>=0 && g[x][y-1]=='1' && vis[x][y-1]!=1){
+   q.push({x,y-1});
+   vis[x][y-1]=1;
             }
-             if(j1+1<g[i1].size() && vis[i1][j1+1]!='1' && g[i1][j1+1]=='1'){
-                q.push({i1,j1+1});
-                 vis[i1][j1+1]='1';
+            if(x+1<g.size() && g[x+1][y]=='1' && vis[x+1][y]!=1){
+                   q.push({x+1,y});
+                   vis[x+1][y]=1;
+
             }
-             if(j1-1>=0 && vis[i1][j1-1]!='1' && g[i1][j1-1]=='1'){
-                q.push({i1,j1-1});
-                 vis[i1][j1-1]='1';
+            if(y+1<g[0].size() && g[x][y+1]=='1' && vis[x][y+1]!=1){
+                   q.push({x,y+1});
+                   vis[x][y+1]=1;
+
             }
-           
 
         }
-
     }
     int numIslands(vector<vector<char>>& g) {
-        vector<vector<char>> vis(g.size(), vector<char>(g[0].size(), '0'));
         int c=0;
+        vector<vector<int>> vis(g.size(),vector<int>(g[0].size(),0));
         for(int i=0;i<g.size();i++){
             for(int j=0;j<g[i].size();j++){
-                if(g[i][j]=='1' && vis[i][j]!='1'){
-                    fun(g,vis,i,j);
+                if(g[i][j]=='1' && vis[i][j]==0){
                     c++;
+                    fun(g,vis,i,j);
+
                 }
             }
         }
