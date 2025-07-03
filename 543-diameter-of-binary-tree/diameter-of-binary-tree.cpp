6176c03d0b6c4b4ec9f5e2217703eb42d54980e6
balaraju <119840117+Balaraju9@users.noreply.github.com>
2025-07-03 12:10:49 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (92.65%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -6,41 +6,24 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-int max1=INT_MIN;
-    int fun(TreeNode* root) {
-        if (root == NULL) {
+    int max1=0;
+    int fun(TreeNode* r){
+        if(r==NULL){
             return 0;
         }
-        int z = 1 + fun(root->left);
-        int z1 = 1 + fun(root->right);
-        return max(z, z1);
+        int l=fun(r->left);
+        int r1=fun(r->right);
+        max1=max(max1,l+r1+1);
+        return 1+max(l,r1);
     }
-    int fun1(TreeNode* root) {
-        if (root == NULL) {
-            return 0;
-        }
-        int z = 1 + fun1(root->left);
-        int z1 = 1 + fun1(root->right);
-        return max(z, z1);
-    }
-    void balu(TreeNode* root){
-        if(root==NULL){
-            return;
-        }
-        max1=max(max1,fun(root->left)+fun(root->right));
-
-        balu(root->left);
-        balu(root->right);
+    int diameterOfBinaryTree(TreeNode* r) {
+        fun(r);
+        return max1-1;
         
-    }
-    int diameterOfBinaryTree(TreeNode* root) {
-        balu(root);
-        return max1;
     }
 };
