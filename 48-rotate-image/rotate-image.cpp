f6f311e5baae4e843c9247d7a9d06999dbadd987
balaraju <119840117+Balaraju9@users.noreply.github.com>
2025-07-03 20:43:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (35.26%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,32 +1,14 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& ar) {
-        int i=0,j=0,c=0;
-        vector<int> v1;
-        vector<vector<int>> v2;
-       while(j<ar[i].size()){
-            v1.push_back(ar[i][j]);
-           i++;
-           c++;
-           
-           if(c==ar.size()){
-               i=0;
-               j++;
-               c=0;
-               reverse(v1.begin(),v1.end());
-               v2.push_back(v1);
-               v1.clear();
-           }
-          
-       }
-      for(int i=0;i<v2.size();i++){
-          for(int j=0;j<v2.size();j++){
-              ar[i][j]=v2[i][j];
-          }
-
-      }
-
-        
+    void rotate(vector<vector<int>>& m) {
+        for(int i=0;i<m.size();i++){
+            for(int j=i+1;j<m[0].size();j++){
+                swap(m[i][j],m[j][i]);
+            }
+        }
+        for(int i=0;i<m.size();i++){
+            reverse(m[i].begin(),m[i].end());
+        }
         
     }
 };
