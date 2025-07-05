052732613854ec6e5c646b94e764a9e85c75795e
balaraju <119840117+Balaraju9@users.noreply.github.com>
2025-07-05 09:52:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (18.20%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
-    int fun(int n, vector<int>& v1) {  // Pass v1 by reference
-        if (n == 0) {
-            return 1;
-        } else if (n < 0) {
-            return 0;
-        }
-        if (v1[n] != -1) return v1[n];
-
-        int l = fun(n - 1, v1);
-        int r = fun(n - 2, v1);
-        v1[n] = l + r;
-        return l + r;
-    }
-    
     int climbStairs(int n) {
-        vector<int> v1(50, -1);  // Initialize the vector
-        return fun(n, v1);
+        vector<int> v1(n+1);
+        v1[0]=1;
+        v1[1]=2;
+        for(int i=2;i<n;i++){
+            v1[i]=v1[i-1]+v1[i-2];
+        }
+        
+        return v1[n-1];
     }
 };
