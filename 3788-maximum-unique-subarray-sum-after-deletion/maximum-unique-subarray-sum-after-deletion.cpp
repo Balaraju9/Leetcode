edd9eb98620438fed83ac084b0e25282e908fdfe
balaraju <119840117+Balaraju9@users.noreply.github.com>
2025-07-25 11:53:14 +0530

    Time: 0 ms (100.00%) | Memory: 29.3 MB (86.99%) - LeetSync

--- 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int maxSum(vector<int>& n) {
-        int max1=INT_MIN;
-        unordered_map<int,int> mp;
+        int max1=*max_element(n.begin(),n.end());
+        set<int> st;
+        int s=INT_MIN;
         for(auto &i:n){
-            if(i>0 && mp.find(i)==mp.end()){
-                mp[i]=1;
-            }
-            else if(i<=0){
-                max1=max(max1,i);
-            }
-        }
-        int ans=-1;
-        for(auto &i:mp){
-            ans+=i.first;
-        }
-        if(ans==-1) return max1;
-        return ans+1;
+            if( i>0 && st.find(i)==st.end()){
+                if(s==INT_MIN){
+                    s=0;
+                }
+                s+=i;
+                st.insert(i);
+                
 
+            }
+            
+        }
+    return max(max1,s);
         
     }
 };
