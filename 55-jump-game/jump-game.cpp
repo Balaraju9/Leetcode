dcc9076c1a1dc80084584f3562f760984c324a58
balaraju <119840117+Balaraju9@users.noreply.github.com>
2026-04-25 16:31:02 +0530

    Time: 3 ms (25.97%) | Memory: 52.2 MB (81.80%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -4,17 +4,18 @@ public:
         if(n.size()<=1){
             return true;
         }
-        vector<bool> v1(n.size(),false);
+        // vector<bool> v1(n.size(),false);
         int reach=n.size()-1;
         for(int i=n.size()-1;i>=0;i--){
             if(i+n[i]>=reach){
-                v1[i]=true;
+                // v1[i]=true;
                 reach=i;
 
 
             }
         }
-        return v1[0];
+        return (n[0]>=reach)?true:false;
+        // return v1[0];
         
     }
 };
